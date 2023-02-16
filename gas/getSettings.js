481988fc788913f0cecfb611f4e3a115e8624267
Brian Hanson <b-hans@umn.edu>
2023-02-16 10:57:10 -0600

    added some settings with folder

--- gas/getSettings.js
+++ gas/getSettings.js
@@ -1,3 +1,7 @@
 function getSettings(){
+
+    var folderNameTest = DriveApp.getFolderById(SETTINGS.siteFolderId);
+    SETTINGS.folderName = folderNameTest.getName();
+    
     return SETTINGS;
 }
